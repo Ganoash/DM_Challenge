ce4b356679f8222203473d37b4024b01493b1ef3
lgkroes <lucas.kroes@hetnet.nl>
2020-12-10 19:40:48 +0100

    Replaced main.py with boilerplate code

--- main.py
+++ main.py
@@ -1,25 +1,105 @@
-import pandas as pd
 import numpy as np
-data_location: str = "Data/"
+import pandas as pd
+import os.path
+from random import randint
 
-movies_filename: str = "movies.csv"
-ratings_filename: str = "ratings.csv"
-users_filename: str = "users.csv"
-predictions_filename: str = "predictions.csv"
-submissions_filename: str = "submissions.csv"
+# -*- coding: utf-8 -*-
+"""
+### NOTES
+This file is an example of what your code should look like. It is written in Python 3.6.
+To know more about the expectations, please refer to the guidelines.
+"""
+
+#####
+##
+# DATA IMPORT
+##
+#####
+
+# Where data is located
+movies_file = './data/movies.csv'
+users_file = './data/users.csv'
+ratings_file = './data/ratings.csv'
+predictions_file = './data/predictions.csv'
+submission_file = './data/submission.csv'
+
+# Read the data using pandas
+movies_description = pd.read_csv(movies_file, delimiter=';', dtype={'movieID': 'int', 'year': 'int', 'movie': 'str'},
+                                 names=['movieID', 'year', 'movie'])
+users_description = pd.read_csv(users_file, delimiter=';',
+                                dtype={'userID': 'int', 'gender': 'str', 'age': 'int', 'profession': 'int'},
+                                names=['userID', 'gender', 'age', 'profession'])
+ratings_description = pd.read_csv(ratings_file, delimiter=';',
+                                  dtype={'userID': 'int', 'movieID': 'int', 'rating': 'int'},
+                                  names=['userID', 'movieID', 'rating'])
+predictions_description = pd.read_csv(predictions_file, delimiter=';', names=['userID', 'movieID'], header=None)
 
 
-def create_utility_matrix(rating_location: str):
-    rating: pd.DataFrame = pd.read_csv(rating_location, sep=';', names=["user", "movie", "rating"])
+#####
+##
+# COLLABORATIVE FILTERING
+##
+#####
 
-    util: pd.DataFrame = rating.pivot(index="user", columns="movie", values="rating").fillna(0).loc[1:, 1:]
+def predict_collaborative_filtering(movies, users, ratings, predictions):
+    # TO COMPLETE
 
-    print(util)
+    pass
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
+#####
+##
+# LATENT FACTORS
+##
+#####
 
-    create_utility_matrix("".join((data_location, ratings_filename)))
+def predict_latent_factors(movies, users, ratings, predictions):
+    # TO COMPLETE
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    pass
+
+
+#####
+##
+# FINAL PREDICTORS
+##
+#####
+
+def predict_final(movies, users, ratings, predictions):
+    # TO COMPLETE
+
+    pass
+
+
+#####
+##
+# RANDOM PREDICTORS
+# //!!\\ TO CHANGE
+##
+#####
+
+# By default, predicted rate is a random classifier
+def predict_random(movies, users, ratings, predictions):
+    number_predictions = len(predictions)
+
+    return [[idx, randint(1, 5)] for idx in range(1, number_predictions + 1)]
+
+
+#####
+##
+# SAVE RESULTS
+##
+#####
+
+# //!!\\ TO CHANGE by your prediction function
+predictions = predict_random(movies_description, users_description, ratings_description, predictions_description)
+
+# Save predictions, should be in the form 'list of tuples' or 'list of lists'
+with open(submission_file, 'w') as submission_writer:
+    # Formates data
+    predictions = [map(str, row) for row in predictions]
+    predictions = [','.join(row) for row in predictions]
+    predictions = 'Id,Rating\n' + '\n'.join(predictions)
+
+    # Writes it dowmn
+    submission_writer.write(predictions)
